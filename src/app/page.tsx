c0235ebc313a9940310ec329d5a44c96f0a0066c
dejwodejo <d.drapinski@proton.me>
2024-04-22 20:53:22 +0200

    connect ui to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,203 @@
 import { playfair_display } from "~/app/fonts";
-import { mockLectures, mockStands } from "~/app/mockData";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export const dynamic = "force-dynamic";
+
+export default async function HomePage() {
+  const [lectures, stands] = await Promise.all([
+    db.query.lectures.findMany({
+      with: {
+        room: true,
+      },
+    }),
+    db.query.stands.findMany(),
+  ]);
+
   return (
     <main>
-        <div className="container mx-auto p-4">
-            <div className="mb-8">
-                <h2 className={ `text-5xl font-semibold my-8 ${ playfair_display.className }` }>{ "Nadchodzące wykłady i warsztaty" }</h2>
-                <div className="w-full flex justify-between items-start gap-7">
-                    <div className="w-1/2 flex flex-col gap-4">
-                        { mockLectures.slice( 0, Math.ceil( mockLectures.length / 2 ) ).map( ( {
-                                                                                                   title,
-                            description,
-                                                                                                   type,
-                                                                                                   start,
-                                                                                                   location,
-                                                                                                   author
-                                                                                               }, index ) => (
-                            <div key={ index }
-                                 className="relative flex flex-col p-4 rounded-2xl border-black border-2 mb-4 min-w-[400px]">
-                                <img src={ type === "lecture" ? "/lecture-icon.png" : "/workshop-icon.png" }
-                                     className={ "absolute top-0 left-0 translate-x-[-50%] translate-y-[-50%] z-[2] size-10 bg-gray-100" }></img>
-                                <div className="flex flex-col justify-between items-baseline mb-2">
-                                    <h3 className={ `font-bold text-3xl ${ playfair_display.className }` }>{ title }</h3>
-                                    <h4 className={ `font-semibold text-2xl ${ playfair_display.className }` }>{ author }</h4>
-                                </div>
-                                <p className="text-sm">{ description }</p>
-                                <div className="flex flex-wrap flex-row gap-2">
-                                    <div className="flex items-center my-2">
-                                        <img src={ "/clock-icon.png" } className={ "size-6 mr-2" }/>
-                                        <p>{ start }</p>
-                                    </div>
-                                    <div className="flex items-center my-2">
-                                        <img src={ "/map-pin-icon.png" } className={ "size-6 mr-2" }/>
-                                        <p>{ location }</p>
-                                    </div>
-                                </div>
-                            </div>
-                        ) ) }
-                    </div>
-                    <div className="w-1/2 flex flex-col gap-4">
-                        { mockLectures.slice( Math.ceil( mockLectures.length / 2 ) ).map( ( {
-                                                                                                title,
-                                                                                                description,
-                                                                                                type,
-                                                                                                start,
-                                                                                                location,
-                                                                                                author
-                                                                                            }, index ) => (
-                            <div key={ index }
-                                 className="relative flex flex-col p-4 rounded-2xl border-black border-2 mb-4 min-w-[400px]">
-                                <img src={ type === "lecture" ? "/lecture-icon.png" : "/workshop-icon.png" }
-                                     className={ "absolute top-0 left-0 translate-x-[-50%] translate-y-[-50%] z-[2] size-10 bg-gray-100" }></img>
-                                <div className="flex flex-col justify-between items-baseline mb-2">
-                                    <h3 className={ `font-bold text-3xl ${ playfair_display.className }` }>{ title }</h3>
-                                    <h4 className={ `font-semibold text-2xl ${ playfair_display.className }` }>{ author }</h4>
-                                </div>
-                                <p>{ description }</p>
-                                <div className="flex flex-wrap flex-row gap-2">
-                                    <div className="flex items-center my-2">
-                                        <img src={ "/clock-icon.png" } className={ "size-6 mr-2" }/>
-                                        <p>{ start }</p>
-                                    </div>
-                                    <div className="flex items-center my-2">
-                                        <img src={ "/map-pin-icon.png" } className={ "size-6 mr-2" }/>
-                                        <p>{ location }</p>
-                                    </div>
-                                </div>
-                            </div>
-                        ) ) }
-                    </div>
-                </div>
-            </div>
-
-
-            <div className="mb-8">
-                <h2 className={ `text-5xl font-semibold mb-4 ${ playfair_display.className }` }>{ "Stanowiska na wydarzeniu" }</h2>
-                { mockStands.map( ( { title, description, location, logo }, index ) => (
-                    <div key={ index } className="flex items-center mb-10">
-                        <div className="flex-grow pr-10">
-                            <h3 className={ `font-bold text-3xl ${ playfair_display.className }` }>{ title }</h3>
-                            <p className="text-sm">{ description }</p>
-                            <div className={ "flex justify-center items-center my-4" }>
-                                <img src={ "/map-pin-icon.png" } alt={ "Gdzie jest stanowisko?" }
-                                     className={ "size-6 mr-2" }/>
-                                <div className={ "flex justify-between items-center w-full" }>
-                                    <p className="text-base">{ location }</p>
-                                    <p className="text-base py-2 px-9 shadow-lg rounded border-2 gray-300 hover:bg-gray-300 hover:border-gray-400">{ "Zobacz na mapie" }</p>
-                                </div>
-                            </div>
+      <div className="container mx-auto p-4">
+        <div className="mb-8">
+          <h2
+            className={`my-8 text-5xl font-semibold ${playfair_display.className}`}
+          >
+            {"Nadchodzące wykłady i warsztaty"}
+          </h2>
+          <div className="flex w-full items-start justify-between gap-7">
+            <div className="flex w-1/2 flex-col gap-4">
+              {lectures
+                .slice(0, Math.ceil(lectures.length / 2))
+                .map(
+                  (
+                    {
+                      title,
+                      description,
+                      type,
+                      start,
+                      end,
+                      authors,
+                      room: { floor, number },
+                    },
+                    index,
+                  ) => (
+                    <div
+                      key={index}
+                      className="relative mb-4 flex min-w-[400px] flex-col rounded-2xl border-2 border-black p-4"
+                    >
+                      <img
+                        src={
+                          type === "traditional"
+                            ? "/lecture-icon.png"
+                            : "/workshop-icon.png"
+                        }
+                        className={
+                          "absolute left-0 top-0 z-[2] size-10 translate-x-[-50%] translate-y-[-50%] bg-gray-100"
+                        }
+                      ></img>
+                      <div className="mb-2 flex flex-col items-baseline justify-between">
+                        <h3
+                          className={`text-3xl font-bold ${playfair_display.className}`}
+                        >
+                          {title}
+                        </h3>
+                        <h4
+                          className={`text-2xl font-semibold ${playfair_display.className}`}
+                        >
+                          {authors}
+                        </h4>
+                      </div>
+                      <p className="text-sm">{description}</p>
+                      <div className="flex flex-row flex-wrap gap-2">
+                        <div className="my-2 flex items-center">
+                          <img
+                            src={"/clock-icon.png"}
+                            className={"mr-2 size-6"}
+                          />
+                          <p>
+                            {"Od " +
+                              start.toLocaleDateString() +
+                              " do " +
+                              end.toLocaleDateString()}
+                          </p>
                         </div>
-                        <img src={ logo } alt="Logo" className="h-56 w-56 ml-4"/>
+                        <div className="my-2 flex items-center">
+                          <img
+                            src={"/map-pin-icon.png"}
+                            className={"mr-2 size-6"}
+                          />
+                          <p>{"Piętro " + floor + " sala " + number}</p>
+                        </div>
+                      </div>
                     </div>
-                ) ) }
+                  ),
+                )}
             </div>
+            <div className="flex w-1/2 flex-col gap-4">
+              {lectures
+                .slice(Math.ceil(lectures.length / 2))
+                .map(
+                  (
+                    {
+                      title,
+                      description,
+                      type,
+                      start,
+                      end,
+                      authors,
+                      room: { floor, number },
+                    },
+                    index,
+                  ) => (
+                    <div
+                      key={index}
+                      className="relative mb-4 flex min-w-[400px] flex-col rounded-2xl border-2 border-black p-4"
+                    >
+                      <img
+                        src={
+                          type === "traditional"
+                            ? "/lecture-icon.png"
+                            : "/workshop-icon.png"
+                        }
+                        className={
+                          "absolute left-0 top-0 z-[2] size-10 translate-x-[-50%] translate-y-[-50%] bg-gray-100"
+                        }
+                      ></img>
+                      <div className="mb-2 flex flex-col items-baseline justify-between">
+                        <h3
+                          className={`text-3xl font-bold ${playfair_display.className}`}
+                        >
+                          {title}
+                        </h3>
+                        <h4
+                          className={`text-2xl font-semibold ${playfair_display.className}`}
+                        >
+                          {authors}
+                        </h4>
+                      </div>
+                      <p>{description}</p>
+                      <div className="flex flex-row flex-wrap gap-2">
+                        <div className="my-2 flex items-center">
+                          <img
+                            src={"/clock-icon.png"}
+                            className={"mr-2 size-6"}
+                          />
+                          <p>
+                            {"Od " +
+                              start.toLocaleDateString() +
+                              " do " +
+                              end.toLocaleDateString()}
+                          </p>
+                        </div>
+                        <div className="my-2 flex items-center">
+                          <img
+                            src={"/map-pin-icon.png"}
+                            className={"mr-2 size-6"}
+                          />
+                          <p>{"Piętro " + floor + " sala " + number}</p>
+                        </div>
+                      </div>
+                    </div>
+                  ),
+                )}
+            </div>
+          </div>
         </div>
+
+        <div className="mb-8">
+          <h2
+            className={`mb-4 text-5xl font-semibold ${playfair_display.className}`}
+          >
+            {"Stanowiska na wydarzeniu"}
+          </h2>
+          {stands.map(
+            ({ name, description, type, location, imageUrl }, index) => (
+              <div key={index} className="mb-10 flex items-center">
+                <div className="flex-grow pr-10">
+                  <h3
+                    className={`text-3xl font-bold ${playfair_display.className}`}
+                  >
+                    {name}
+                  </h3>
+                  <p className="text-sm">{description}</p>
+                  <div className={"my-4 flex items-center justify-center"}>
+                    <img
+                      src={"/map-pin-icon.png"}
+                      alt={"Gdzie jest stanowisko?"}
+                      className={"mr-2 size-6"}
+                    />
+                    <div className={"flex w-full items-center justify-between"}>
+                      <p className="text-base">{location}</p>
+                      <p className="gray-300 rounded border-2 px-9 py-2 text-base shadow-lg hover:border-gray-400 hover:bg-gray-300">
+                        {"Zobacz na mapie"}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <img src={imageUrl} alt="Logo" className="ml-4 h-56 w-56" />
+              </div>
+            ),
+          )}
+        </div>
+      </div>
     </main>
   );
 }
