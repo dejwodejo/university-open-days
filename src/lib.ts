e57bb90da04a83f3cffbe384577545b0dedbdda4
dejwodejo <d.drapinski@proton.me>
2024-06-12 23:59:25 +0200

    fix session token expire date format

--- src/lib.ts
+++ src/lib.ts
@@ -23,7 +23,7 @@ export async function encrypt(payload: UserSession): Promise<string> {
   })
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime("day")
+    .setExpirationTime("1d")
     .sign(key);
 }
 
